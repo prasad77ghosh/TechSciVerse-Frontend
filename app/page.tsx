9eee7ebe13f9573c49c2a58ec1c5a652b747d456
prasad77ghosh <prasadghosh997@gmail.com>
2023-05-06 20:41:50 +0530

    this is first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hyy I am prasad</h1>
+    </div>
+  );
+}
---
+++ components/clients/Navbar.tsx
@@ -0,0 +1,11 @@
+import { BsSearch } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
